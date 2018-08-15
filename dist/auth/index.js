9c2fa00025aa72b025d50178e60a2deb70c133f9
Héctor J. Vásquez <ipi.vasquez@gmail.com>
2018-08-15 14:04:06 -0500

    Removing dist
    Removing dist from repository: as it should be.

--- dist/auth/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var authenticator_1 = require("api/auth/config/authenticator");
-exports.authenticator = authenticator_1.authenticator;
-var module_1 = require("api/auth/module");
-exports.auth = module_1.auth;
